136af26b185972d90a37fad6e2e66f02fe0f8b1d
FelixCpp <fix.busch@gmail.com>
2024-03-07 16:29:46 +0100

    Project created

---
+++ premake5.lua
@@ -0,0 +1,21 @@
+workspace "LINQ-Workspace"
+    configurations { "Debug", "Release" }
+    architecture "x86_64"
+    flags { "MultiProcessorCompile" }
+
+project "LINQ"
+    location "LINQ"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "bin/%{cfg.buildcfg}"
+    files { "LINQ/lib/**.hpp", "LINQ/Main.cpp" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
